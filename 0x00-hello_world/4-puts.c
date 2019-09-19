98d0ffcc80e8a03ec617132b6caaacc77baa91da
Ashlee <827@holbertonschool.com>
2019-09-19 21:41:09 +0000

    Accidentally deleted and readded

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - is printing out a string from the command puts
+ *
+ * Return: 0 for it to work
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
