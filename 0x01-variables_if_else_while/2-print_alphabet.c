2bc87b485032cec49c2577166f946513ca94d13e
Ashlee <827@holbertonschool.com>
2019-09-20 17:35:16 +0000

    Using C code, prints the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints a random number as positive, zero, or negative depending
+ *on relation to zero
+ *Return: returns 0
+ */
+
+int main (void)
+{
+	char a;
+	for(a = 'a'; a <= 'z'; a++) {
+		putchar(a);
+		putchar('\n');
+			}
+	return 0;
+}
